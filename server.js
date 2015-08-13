461f87589d6f1a62bc08cc89a8a8ec70a5d13fba
Vicki Cole <vicki@capturecadet.com>
2015-08-13 18:02:26 +0100

    Add pad view

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
+var path = require('path');
+
 var express = require('express');
 var app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(express.static(__dirname + '/public'));
+// public folder to store assets
+app.use(express.static(path.join(__dirname, 'public')));
+
+// routes
+app.get('/', function(req, res) {
+  res.render('pad');
+});
+
+// listen on port 8000
+var port = process.env.PORT || 8000;
+app.listen(port);
